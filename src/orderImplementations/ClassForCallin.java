e992ad926c89682096db5c2ced086ac7f0f6eb46
apnijava <vijaydehraj15@gmail.com>
2023-02-24 18:05:46 +0530

    added some comments

--- src/orderImplementations/ClassForCallin.java
+++ src/orderImplementations/ClassForCallin.java
@@ -6,7 +6,7 @@ public class ClassForCallin {
 	public static void classforcall() {
 		int choice, subcho;
 		Scanner in = new Scanner(System.in);
-		System.out.println("**MAIN CATEGORIES**\n");
+		System.out.println("\n**MAIN CATEGORIES**\n");
 		ShowAllCategory.print();
 		// Taking input fro the main category
 		choice = in.nextInt();
@@ -18,29 +18,31 @@ public class ClassForCallin {
 			System.out.println(" Press 1 to see the price comparision in region other than India.");
 			System.out.println(" Press 2 to see the Highest and lowest prices of Laptops.");
 			System.out.println(" Press 3 to see the total Stock of Laptops.");
-			System.out.println("Press 4 to sort the products");
+			System.out.println(" Press 4 to sort the products");
 			// Taking Input for the sub options.
 			subcho = in.nextInt();
 			switch (subcho) {
 			case 1:
-				ShowData.Currency("Laptops");
+				ShowData.Currency("laptops");
 				break;
 			case 2:// select Max(price) from tablename; and Min(price) from tablename;
-				MaxAndMin.maxPrice("Laptops");
-				MaxAndMin.minPrice("Laptops");
+				MaxAndMin.maxPrice("laptops");
+				MaxAndMin.minPrice("laptops");
 				break;
 			case 3://select SUM(QUANTITY) from tablename;
-				MaxAndMin.sum("Laptops");
+				MaxAndMin.sum("laptops");
+				MaxAndMin.maxQuant("laptops");
+				MaxAndMin.minQuant("laptops");
 				break;// case 3.
 			case 4:
 				System.out.println("Sort by Name press --> 1.\nSort by Price press -->2.");
 				subcho = in.nextInt();
 				switch (subcho) {
 				case 1://select * from tablename ORDER BY name.
-					Sorting.sortByName("Laptops");
+					Sorting.sortByName("laptops");
 					break;
 				case 2://select * from tablename ORDER BY price.
-					Sorting.sortByPrice("Laptops");
+					Sorting.sortByPrice("laptops");
 					break;
 				default:
 					System.out.println("Wrong Input!");
@@ -60,7 +62,7 @@ public class ClassForCallin {
 			System.out.println(" Press 1 to see the price comparision in region other than India.");
 			System.out.println(" Press 2 to see the Highest and lowest prices of Headphones.");
 			System.out.println(" Press 3 to see the total Stock of Headphones.");
-			System.out.println("Press 4 to sort the products");
+			System.out.println(" Press 4 to sort the products");
 			// Taking Input for the sub options.
 			subcho = in.nextInt();
 			switch (subcho) {
@@ -73,7 +75,8 @@ public class ClassForCallin {
 				break;
 			case 3://select SUM(QUANTITY) from tablename;
 				MaxAndMin.sum("Headphones");
-				
+				MaxAndMin.maxQuant("Headphones");
+				MaxAndMin.minQuant("Headphones");
 				break;// case 3 break.
 			case 4:
 				System.out.println("Sort by Name press --> 1.\nSort by Price press -->2.");
@@ -103,7 +106,7 @@ public class ClassForCallin {
 			System.out.println(" Press 1 to see the price comparision in region other than India.");
 			System.out.println(" Press 2 to see the Highest and lowest prices of Watches.");
 			System.out.println(" Press 3 to see the total Stock of Watches.");
-			System.out.println("Press 4 to sort the products");
+			System.out.println(" Press 4 to sort the products");
 			// Taking Input for the sub options.
 			subcho = in.nextInt();
 			switch (subcho) {
@@ -116,6 +119,8 @@ public class ClassForCallin {
 				break;
 			case 3://select SUM(QUANTITY) from tablename;
 				MaxAndMin.sum("Watches");
+				MaxAndMin.maxQuant("Watches");
+				MaxAndMin.minQuant("Watches");
 				break;// case 3 break.
 			case 4:
 				System.out.println("Sort by Name press --> 1.\nSort by Price press -->2.");
@@ -137,7 +142,7 @@ public class ClassForCallin {
 				break;// switch break.
 			}
 			break;//
-		case 4:
+		case 4:// using insert into statement.
 			RequiredCategories.addCategories();
 			break;
 		default:// main category default.
@@ -145,6 +150,5 @@ public class ClassForCallin {
 
 			break;// main category switch break
 		}
-
 	}
 }
