e5cd95047343d4ea3b94b865bc67c84699c976cb
apnijava <vijaydehraj15@gmail.com>
2023-02-24 18:09:00 +0530

    added some comments

--- src/orderImplementations/ShowData.java
+++ src/orderImplementations/ShowData.java
@@ -11,26 +11,26 @@ public class ShowData {
 	public static void showdata(String tableName) {
 		try {
 			Connection con = ConnectionProvider.getConnection();
-			// Statement stmt = con.createStatement()
+			
 
 			PreparedStatement ps = con.prepareStatement("select * from " + tableName);
 
 			ResultSet rs = ps.executeQuery();
-			System.out.printf(
-					"---------------------------------------------------------------%n");
-			System.out.printf("Procuts Name\t\t|\tPrice In Rs.\t|\n");
-			System.out.printf(
-					"--------------------------------------------------------------%n");
+			System.out.printf("-----------------------------------------------------------------------------------------------%n");
+			System.out.printf("Product ID\t\t|\tProdcut Name\t\t|\tPrice In Rs.\t|\n");
+			System.out.printf("-------------------------------------------------------------------------------------------------%n");
 			while (rs.next()) {
-				System.out.printf("\t%-14s\t\t%-10s\t\t\t%n", rs.getString(1), rs.getInt(2)+" ₹-/");
-
+				
+				  System.out.printf("\t%-14s\t\t%-14s\t\t\t%-14s\t\t\t\t%n", rs.getInt(1),
+				  rs.getString(2), rs.getInt(3) + " ₹-/");
+				 
 			}
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 
 	}
-
+// method for the currency conversion for region other than INDIA.
 	public static void Currency(String tableName) {
 		try {
 			Connection con = ConnectionProvider.getConnection();
@@ -42,13 +42,14 @@ public class ShowData {
 			System.out.printf(
 					"-----------------------------------------------------------------------------------------------------------------------------------------------------------------%n");
 			System.out
-					.printf("Procuts Name\t\t|\tPrice In Rs.\t\t|\t US(Dollar)\t\t|\tFRANCE(Euro)\t\t|\tUK(Pound)|\n");
+					.printf("Procuts Name\t|\tPrice In Rs.\t|\t US(Dollar)\t|\tFRANCE(Euro)\t|\tUK(Pound)|\n");
 			System.out.printf(
 					"-----------------------------------------------------------------------------------------------------------------------------------------------------------------%n");
 			while (rs.next()) {
-				System.out.printf("\t%-14s\t\t%-10s\t\t\t%-12s\t\t\t%-14s\t\t\t\t%-14s\t\t\t\t\t%n", rs.getString(1),
-						rs.getInt(2) + " ₹", "$ " + (rs.getInt(2) / 82), "€ " + rs.getInt(2) / 80,
-						"£ " + rs.getInt(2) / 99);
+				// converting the currency according to countries US,FRANCE,UK etc.
+				System.out.printf("\t%-14s\t\t%-10s\t\t\t%-10s\t\t\t%-11s\t\t\t\t%-6s\t\t\t\t\t%n", rs.getString(2),
+						rs.getInt(3) + " ₹", "$ " + (rs.getInt(3) / 82), "€ " + rs.getInt(3) / 80,
+						"£ " + rs.getInt(3) / 99);
 
 			}
 		} catch (Exception e) {
